d38dc58810c3ce1dff647037b56378a6cb2133bd
Alvin Kato J.R <alvinkatojr@gmail.com>
2013-11-27 22:00:50 +0300

    Add $ sign processing on Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
   validates_numericality_of :price
   validates :stock, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def price=(input)
+    input.delete!("$")
+    super
+  end
 end
