84fdc69f5cbfa593b4a85b239393d0b2244a1084
Alvin Kato J.R <alvinkatojr@gmail.com>
2013-11-27 16:18:24 +0300

    Add extra options to price column

--- db/migrate/20131127131524_create_products.rb
+++ db/migrate/20131127131524_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :title
-      t.decimal :price, precison:8, scale:2
+      t.decimal :price, precision:8, scale:2
       t.text :description
       t.string :image_url
 
