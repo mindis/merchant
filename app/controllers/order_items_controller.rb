9bed5fdb5f7a05a6dec96d74788637ec88f13833
Alvin Kato J.R <alvinkatojr@gmail.com>
2013-11-28 03:21:21 +0300

    Add new parameters to create action

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -25,11 +25,11 @@ class OrderItemsController < ApplicationController
   # POST /order_items
   # POST /order_items.json
   def create
-    @order_item = OrderItem.new(order_item_params)
+    @order_item = OrderItem.new(product_id: params[:product_id], order_id: @order.id)
 
     respond_to do |format|
       if @order_item.save
-        format.html { redirect_to @order_item, notice: 'Order item was successfully created.' }
+        format.html { redirect_to @order, notice: 'Successfully added product to cart.' }
         format.json { render action: 'show', status: :created, location: @order_item }
       else
         format.html { render action: 'new' }
@@ -72,7 +72,7 @@ class OrderItemsController < ApplicationController
         session[:order_id] = @order.id
       end
     end
-    
+
     # Use callbacks to share common setup or constraints between actions.
     def set_order_item
       @order_item = OrderItem.find(params[:id])
