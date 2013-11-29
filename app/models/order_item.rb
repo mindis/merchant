baf8576dbb6bcf704c737dbfe471e10346114d14
Alvin Kato J.R <alvinkatojr@gmail.com>
2013-11-30 02:16:24 +0300

    Add validations to order_items model

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,6 +3,7 @@ class OrderItem < ActiveRecord::Base
   belongs_to :product
 
   validates :product, :order, presence: true
+  validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0}
 
   def subtotal
     quantity * product.price
