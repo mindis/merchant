4f88827c15fccc24b8a8eebda1d9b6032966fbd7
Alvin Kato J.R <alvinkatojr@gmail.com>
2013-12-03 23:34:45 +0300

    Add 2nd parameter to stock status helper

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -4,7 +4,7 @@ module ProductsHelper
     number_to_currency price
   end
 
-  def print_stock(stock, requested)
+  def print_stock(stock, requested = 1)
     if stock == 0
       content_tag(:span, "Out of Stock", class: "out_stock")
     elsif stock >= requested
